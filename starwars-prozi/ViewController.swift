b5104b9a814dc97b41d97bd858f8c138c422dc36
Rodrigo <rodrigo.scroferneker@gmail.com>
2021-11-30 16:18:20 -0300

    Initial Commit

---
+++ starwars-prozi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  starwars-prozi
+//
+//  Created by Rodrigo Franzoi on 30/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ starwars-proziTests/starwars_proziTests.swift
@@ -0,0 +1,33 @@
+//
+//  starwars_proziTests.swift
+//  starwars-proziTests
+//
+//  Created by Rodrigo Franzoi on 30/11/21.
+//
+
+import XCTest
+@testable import starwars_prozi
+
+class starwars_proziTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
